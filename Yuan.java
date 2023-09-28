154613aad59413c626770f45e845eec7ee283bbf
sinkudo <101506877+sinkudo@users.noreply.github.com>
2023-09-28 18:55:24 +0800

    Delete Yuan.java

--- Yuan.java
+++
@@ -1,9 +0,0 @@
-public class Yuan extends Currency{
-    public Yuan(int units){
-        super(units, "yuan");
-    }
-    @Override
-    float Value() {
-        return units * 0.136645f;
-    }
-}
