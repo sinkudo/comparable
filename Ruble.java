8ebe06635ccfab4e0aafd5790fd697ddb015325a
sinkudo <101506877+sinkudo@users.noreply.github.com>
2023-09-28 18:55:16 +0800

    Delete Ruble.java

--- Ruble.java
+++
@@ -1,9 +0,0 @@
-public class Ruble extends Currency{
-    public Ruble(int units){
-        super(units, "ruble");
-    }
-    @Override
-    float Value() {
-        return units * 0.0103f;
-    }
-}
